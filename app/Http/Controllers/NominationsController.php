37e332f8c327b983401b8d0fe64124c2d60b0dfc
SedlarDavid <sedlardavid.cz@gmail.com>
2023-03-12 14:12:42 +0100

    return faculties

--- app/Http/Controllers/NominationsController.php
+++ app/Http/Controllers/NominationsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Faculty;
 use App\Models\Nomination;
 use Illuminate\Http\Request;
 
@@ -9,7 +10,10 @@ class NominationsController extends Controller
 {
     public static function index()
     {
-        return Nomination::all();
+        $faculties = Faculty::all();
+        return response()->json(array(
+            'faculties' => $faculties->toArray()
+        ));
     }
 
     public function store(Request $request)
